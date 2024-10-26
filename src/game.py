75cba178304ba83665c0db122c3316c1048dc5e0
maritza1818 <maritzaclaudiamarronpuma@gmail.com>
2024-10-25 19:37:46 -0500

    Add letter input loop and append guessed letters

--- src/game.py
+++ src/game.py
@@ -1,4 +1,9 @@
 import random
-words = {"meow", "cats", "claudita", "aea"}
+words = ["meow", "cats", "claudita", "aea"]
 word = random.choice(words)
 guessed_letters = []
+while True:
+    letter = input("Ingrese su letra correspondiente: ")
+    guessed_letters.append(letra)
+    hidden_letter=''
+    
